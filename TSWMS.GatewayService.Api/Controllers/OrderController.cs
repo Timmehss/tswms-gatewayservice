2160068df8e322c5257857a406aee46a2ce89b53
Tim <timspitters@hotmail.nl>
2025-04-11 12:57:20 +0200

    add order controller

---
+++ TSWMS.GatewayService.Api/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TSWMS.GatewayService.Api.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
